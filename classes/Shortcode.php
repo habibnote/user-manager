0ae63de87372a01eca4cba6c5fdc99b92402d0d5
Habibur Rahman <habibblog97@gmail.com>
2024-10-23 21:52:38 +0600

    login form done

--- classes/Shortcode.php
+++ classes/Shortcode.php
@@ -4,10 +4,34 @@ namespace Habib\UserManager\Classes;
 
 class Shortcode {
     public function login() {
-        return "Login";
+        ob_start();
+        ?>
+            <form action="">
+                <p>
+                    <label for="um-login-email">Email</label>
+                    <input type="email" id="um-login-email" placeholder="Enter Your Email">
+                </p>
+                <p>
+                    <label for="um-login-password">Password</label>
+                    <input type="email" id="um-login-password" placeholder="Enter Your Password">
+                </p>
+                <p>
+                    <a href="#">Forget Password</a>
+                </p>
+                <p>
+                    <button type="submit">Login</button>
+                </p>
+
+            </form>
+        <?php
+        return ob_get_clean();
     }
 
     public function registration() {
-        return "Registration";
+        ob_start();
+        ?>
+            
+        <?php
+        return ob_get_clean();
     }
 }

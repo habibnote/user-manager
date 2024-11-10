4ef688ba2f4369de523661abe5c13e1add0bfa2a
Habibur Rahman <habibblog97@gmail.com>
2024-11-10 22:38:46 +0600

    misc

--- view/user/registration-form.php
+++ view/user/registration-form.php
@@ -1,32 +1,32 @@
 <?php 
     if ( isset( $_POST['um_submit_registration'] ) ) {
-        
+            
         $name       = isset( $_POST['um_signup_name'] ) ? sanitize_text_field( $_POST['um_signup_name'] ) : '';
         $email      = isset( $_POST['um_signup_email'] ) ? sanitize_email( $_POST['um_signup_email'] ) : '';
         $country    = isset( $_POST['um_signup_country'] ) ?  sanitize_text_field( $_POST['um_signup_country'] ) : '';
         $password   = isset( $_POST['um_signup_password'] ) ?  sanitize_text_field( $_POST['um_signup_password'] ) : '';
         $confirm_password = isset( $_POST['um_signup_confirm_password'] ) ? sanitize_text_field( $_POST['um_signup_confirm_password'] ) : '';
-    
+
         if ( empty( $name ) || empty( $email ) || empty( $password ) || empty( $confirm_password ) || $password !== $confirm_password ) {
             esc_html_e( 'Please fill in all fields correctly.', 'u-manager');
-            return;
-        }
+        }else {
+            // Create the user
+            $user_id = wp_create_user( $email, $password, $email );
+            if ( ! is_wp_error( $user_id ) ) {
+                
+                wp_update_user( array( 'ID' => $user_id, 'display_name' => $name ) );
+                update_user_meta( $user_id, 'country', $country );
     
-        // Create the user
-        $user_id = wp_create_user( $email, $password, $email );
-        if ( ! is_wp_error( $user_id ) ) {
-            
-            wp_update_user( array( 'ID' => $user_id, 'display_name' => $name ) );
-            update_user_meta( $user_id, 'country', $country );
+                printf( "<script>window.location.href = '%s?user_id=%s';</script>", site_url('/dashboard'), $user_id );
+    
+            } else {
+                echo 'Error: ' . $user_id->get_error_message();
+            }
+        }
 
-            printf( "<script>window.location.href = '%s?user_id=%s';</script>", site_url('/dashboard'), $user_id );
-    
-        } else {
-            echo 'Error: ' . $user_id->get_error_message();
-        }
     }
-    
 ?>
+
 <form method="POST">
     <p>
         <label for="um-signup-name">

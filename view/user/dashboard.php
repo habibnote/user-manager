7c8ebd1bd86eddeebae20e636d20d0e3657b73c6
Habibur Rahman <habibblog97@gmail.com>
2024-11-10 23:12:37 +0600

    misc

--- view/user/dashboard.php
+++ view/user/dashboard.php
@@ -1,11 +1,31 @@
+<?php $url = get_permalink(); ?>
 <div class="um-user-dashboard-wrapper">
     <h1>User Dashboard</h1>
     <nav class="um-navigation"> 
         <ul>
-            <li><a href="#">New Order</a></li>
-            <li><a href="#">My Service</a></li>
-            <li><a href="#">Renew</a></li>
-            <li><a href="#">Earn</a></li>
+            <?php 
+                $dashboard_options = [
+                    [
+                        'title'     => 'New Order',
+                        'status'    => 'new-order'
+                    ],
+                    [
+                        'title'     => 'My Service',
+                        'status'    => 'my-service'
+                    ],
+                    [
+                        'title'     => 'Renew',
+                        'status'    => 'renew'
+                    ],
+                    [
+                        'title'     => 'Earn',
+                        'status'    => 'earn'
+                    ],
+                ];
+                foreach( $dashboard_options as $options ) {
+                    printf( '<li><a href="%s">%s</a></li>', add_query_arg( 'status', $options['status'], $url ), $options['title'] );
+                }   
+            ?>
         </ul>
     </nav>
 </div>
